9557c88bc5427814768aea4f293d12b448c841f3
malikabhinav31 <malikabhinav31@gmail.com>
2018-06-28 00:11:43 +0530

    Autoscrolling added

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
-	var socket=io();
+var socket=io();
 
+function scroll(){
+  var messages=jQuery('#messages');
+  var newMessage=messages.children('li:last-child');
+
+  var clientHeight=messages.prop('clientHeight');
+  var scrollHeight=messages.prop('scrollHeight');
+  var scrollTop=messages.prop('scrollTop');
+  var newMessageHeight=newMessage.innerHeight();
+  var lastMessageHeight=newMessage.prev().innerHeight();
+
+  if(clientHeight+scrollTop+newMessageHeight+lastMessageHeight>=scrollHeight)
+  {
+  	
+  	messages.scrollTop(scrollHeight);
+  }
+};
 socket.on('connect',function () {
    		console.log('connected to server');
  
@@ -15,6 +31,7 @@ socket.on('newMessage',function(message){
     });
 
     jQuery('#messages').append(html);
+    scroll();
 });
 
 socket.on('newLocationMessage',function(message){
@@ -28,6 +45,7 @@ socket.on('newLocationMessage',function(message){
     });
 
     jQuery('#messages').append(html);
+    scroll();
 });
  socket.on('disconnect',function () {
    console.log('Disconnected from server');
