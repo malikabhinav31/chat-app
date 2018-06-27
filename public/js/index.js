d62311f88333ef32656ad9cd38c316d8a8f3c537
malikabhinav31 <malikabhinav31@gmail.com>
2018-06-27 19:22:30 +0530

    templates using mustachejs

--- public/js/index.js
+++ public/js/index.js
@@ -6,25 +6,28 @@ socket.on('connect',function () {
    });
 
 socket.on('newMessage',function(message){
-   console.log("New message received",message);
-    
     var formattedTime=moment(message.createdAt).format('h:mm a');
-    var li=jQuery('<li></li>');
-    li.text(`${message.from} ${formattedTime}:  ${message.text}`);
+    var template=jQuery('#message-template').html();
+    var html=Mustache.render(template,{
+    	from:message.from,
+    	text:message.text,
+    	createdAt:formattedTime
+    });
 
-    jQuery('#messages').append(li);
+    jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage',function(message){
  
-    var li=jQuery('<li></li>');
-    var a=jQuery('<a target="_blank">My Current Location</a>');
-
     var formattedTime=moment(message.createdAt).format('h:mm a');
-    li.text(`${message.from}  ${formattedTime}: `);
-    a.attr('href',message.url);
-    li.append(a);
-    jQuery('#messages').append(li);
+    var template=jQuery('#location-template').html();
+    var html=Mustache.render(template,{
+    	from:message.from,
+    	url:message.url,
+    	createdAt:formattedTime
+    });
+
+    jQuery('#messages').append(html);
 });
  socket.on('disconnect',function () {
    console.log('Disconnected from server');
